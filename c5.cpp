87c2da750cd71afbf87f7fc308eefff93395fc53
Santiago Sanchez <santiago.sanchez.t@gmail.com>
2025-07-30 23:29:15 -0500

    feat: Add partial implementation of c5 challenge
    feat: Add partial implementation of c5 challenge
    
    feat: Add partial implementation of c5 challenge

--- c5.cpp
+++ c5.cpp
@@ -1,9 +1,12 @@
+#include <cctype>
 #include <iostream>
 #include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::tolower;
+using std::vector;
 
 /*
  * Challenge: The challenge is about to create a list of integrers allowing the
@@ -20,6 +23,83 @@ using std::endl;
  *
  * Enter your choice:
  *
+ * The program should only accept valid choices from the user, both upper and
+ * lower cases should be allowed. If an illegal choice is made, you should
+ * display 'Unknow selection, please try again.' and the menu options should be
+ * displayed again.
  *
+ * Some additional functionalities are:
+ * - Search for a number and if found display the number of times it occurs in
+ * the list
+ * - Clearing out the list
  */
-int main() {}
+
+void print_numbers(vector<int> arr) {
+
+  for (auto number : arr) {
+    cout << number << ", ";
+  }
+  cout << endl;
+}
+
+void add_number(vector<int> *arr, int new_number) {
+
+  arr->push_back(new_number);
+  cout << "Number " << new_number << " was successfully added" << endl;
+  cout << endl;
+}
+
+int main() {
+
+  int app_flag = 1;
+  vector<int> numbers;
+
+  do {
+
+    cout << "=================================" << endl;
+    cout << "Program Menu: " << endl;
+    cout << "=================================" << endl;
+    cout << "P - Print numbers" << endl;
+    cout << "A - Add number" << endl;
+    cout << "M - Display the mean of numbers" << endl;
+    cout << "S - Display the smallest number" << endl;
+    cout << "L - Display the largest number" << endl;
+    cout << "Quit" << endl;
+    cout << "=================================" << endl;
+    cout << "Enter yout choice: ";
+
+    char choice_menu;
+    cin >> choice_menu;
+
+    switch (tolower(choice_menu)) {
+    case 'p':
+
+      print_numbers(numbers);
+      break;
+    case 'a':
+
+      int new_number;
+      cout << "Enter number to be added: " << endl;
+      cin >> new_number;
+      add_number(&numbers, new_number);
+      break;
+    case 'm':
+      cout << "option p" << endl;
+      break;
+    case 's':
+      cout << "option p" << endl;
+      break;
+    case 'l':
+      cout << "option p" << endl;
+      break;
+    case 'q': {
+      cout << "Good Bye!";
+      app_flag = 0;
+      break;
+    }
+    default:
+      cout << "Invalid option, please try again." << endl;
+    }
+
+  } while (app_flag != 0);
+}
