8122fabe2539ce8388f41cc5a1837871e75315ba
Santiago Sanchez <santiago.sanchez.t@gmail.com>
2025-07-31 21:18:12 -0500

    feat: remaining implementations for c5

--- c5.cpp
+++ c5.cpp
@@ -1,10 +1,12 @@
 #include <cctype>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::numeric_limits;
 using std::tolower;
 using std::vector;
 
@@ -49,6 +51,55 @@ void add_number(vector<int> *arr, int new_number) {
   cout << endl;
 }
 
+float mean_arr(vector<int> arr) {
+
+  float mean{0};
+
+  for (auto elem : arr) {
+
+    mean += elem;
+  }
+
+  mean = mean / arr.size();
+
+  return mean;
+}
+
+int smalles_value(vector<int> arr) {
+
+  int smallest{numeric_limits<int>::max()};
+  for (auto elem : arr) {
+
+    if (elem < smallest)
+      smallest = elem;
+  }
+
+  return smallest;
+}
+
+int biggest_value(vector<int> arr) {
+
+  int biggest_value{numeric_limits<int>::min()};
+  for (auto elem : arr) {
+
+    if (elem > biggest_value)
+      biggest_value = elem;
+  }
+
+  return biggest_value;
+}
+
+int number_times(vector<int> arr, int number_to_find) {
+
+  int times{0};
+  for (auto elem : arr) {
+    if (elem == number_to_find)
+      times++;
+  }
+
+  return times;
+}
+
 int main() {
 
   int app_flag = 1;
@@ -64,7 +115,8 @@ int main() {
     cout << "M - Display the mean of numbers" << endl;
     cout << "S - Display the smallest number" << endl;
     cout << "L - Display the largest number" << endl;
-    cout << "Quit" << endl;
+    cout << "T - Display number of times a number is repeated" << endl;
+    cout << "Q - Quit" << endl;
     cout << "=================================" << endl;
     cout << "Enter yout choice: ";
 
@@ -83,15 +135,35 @@ int main() {
       cin >> new_number;
       add_number(&numbers, new_number);
       break;
-    case 'm':
-      cout << "option p" << endl;
+    case 'm': {
+
+      float mean_total = mean_arr(numbers);
+      cout << "The mean is: " << mean_total << endl;
       break;
-    case 's':
-      cout << "option p" << endl;
+    }
+    case 's': {
+
+      int small_number = smalles_value(numbers);
+      cout << "The smallest number of the array is: " << small_number << endl;
+
       break;
-    case 'l':
-      cout << "option p" << endl;
+    }
+    case 'l': {
+
+      int biggest = biggest_value(numbers);
+      cout << "The biggest number of the array is: " << biggest << endl;
       break;
+    }
+    case 't': {
+
+      int number_to_find;
+      cout << "Enter the number to find: " << endl;
+      cin >> number_to_find;
+      int number_repeated_times = number_times(numbers, number_to_find);
+      cout << "The number " << number_to_find << " has been found "
+           << number_repeated_times << " times" << endl;
+      break;
+    }
     case 'q': {
       cout << "Good Bye!";
       app_flag = 0;
